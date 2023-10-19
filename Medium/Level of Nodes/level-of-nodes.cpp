190524f824c1e9eef9edd00140deeb0e305e22a2
Rishikaathane108 <91424358+Rishikaathane108@users.noreply.github.com>
2023-10-19 11:40:32 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Level of Nodes/level-of-nodes.cpp
@@ -0,0 +1,66 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution
+{
+	public:
+	//Function to find the level of node X.
+	int nodeLevel(int V, vector<int> adj[], int X) 
+	{
+	    // code here
+	    queue<pair<int, int>> q;
+	    int curNode, curLvl;
+	    unordered_map<int, int> m;
+	    
+	    q.push({0, 0}); // node, level
+	    while(q.size()) {
+	        curNode = q.front().first, curLvl = q.front().second;
+	        q.pop();
+	        m[curNode]++;
+	        
+	        for(int nbr : adj[curNode]) {
+	            if(nbr == X) return curLvl + 1;
+	            if(!m[nbr]) q.push({nbr, curLvl + 1});
+	        }
+	    }
+	    
+	    return -1;
+	}
+};
+
+//{ Driver Code Starts.
+
+
+int main()
+{
+    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+    	int V, E, X;
+    	cin >> V >> E;
+
+    	vector<int> adj[V];
+
+    	for(int i = 0; i < E; i++)
+    	{
+    		int u, v;
+    		cin >> u >> v;
+    		adj[u].push_back(v);
+    		adj[v].push_back(u);
+    	}
+    	cin>>X;
+
+    	Solution obj;
+    	cout << obj.nodeLevel(V, adj, X) << "\n";
+    }
+
+    return 0;
+}
+
+
+// } Driver Code Ends
